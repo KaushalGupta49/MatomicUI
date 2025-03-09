5a2c77e96b471b375c5ae3b643bc2b7c94cacf1e
Kaushal Gupta <kaushal.gupta@dhani.com>
2025-03-09 20:25:03 +0530

    updates

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,24 +1,10 @@
-import ClampText from "./Clamp";
+import { Slider } from "./Components/ts/Slider";
 
 export default function App() {
   return (
-    <div className="p-6">
-      <ClampText minSize={14} maxSize={20}>
-        This text scales from 14px to 20px.
-      </ClampText>
-      <ClampText minSize={16} maxSize={32}>
-        This text scales from 16px to 32px.
-      </ClampText>
-      <ClampText minSize={18} maxSize={40}>
-        This text scales from 18px to 40px.
-      </ClampText>
-
-      <ClampText minSize={16} maxSize={32} scale={3}>
-        Faster scaling (3vw).
-      </ClampText>
-      <ClampText minSize={16} maxSize={32} scale={1}>
-        Slower scaling (1vw).
-      </ClampText>
-    </div>
+    <>
+      <Slider />
+      <Slider min={1} max={100} step={100} value={10} />
+    </>
   );
 }
